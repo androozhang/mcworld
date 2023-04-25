8ec4e01e3c53e4c8352498425d081f571eb59972
Andrew Zhang <zandrew129@gmail.com>
2023-04-25 19:59:26 -0400

    Changed css

--- src/pages/ReadPosts.js
+++ src/pages/ReadPosts.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
 import { supabase } from '../client';
+import './ReadPosts.css';
 
 const ReadPosts = (props) => {
 
